9c678021a8bb7baa202bdb18acdf918a98084ace
Cody Bellec <Cody_Bellec@student.uml.edu>
2020-10-01 23:32:38 -0400

    packages/homework3/src/homework3.py

--- packages/homework3/src/homework3.py
+++ packages/homework3/src/homework3.py
@@ -4,24 +4,25 @@ import rospy
 from std_msgs.msg import Float32
 
 class Homework3:
-	def __init__(self):
-		rospy.Subscriber("/homework1/total", Float32, self.callback)
-		self.pub = rospy.Publisher("/homework3/converted_total", Float32, queue_size=10)
-
-	def callback(self, unit):
-		if rospy.has_param("/value"):
-			self.mode = rospy.get_param("/value")
-		else:
-    			self.mode = "meters"
-    		if self.mode == "smoots":
-    			turnout = data.data * 1.7018
-    		elif self.mode == "feet":
-    			turnout = data.data
-    		else:
-    			turnout = data.data * 3.2808
+    def __init__(self):
+        rospy.Subscriber("/homework1/total", Float32, self.callback)
+        self.pub = rospy.Publisher("/homework3/converted_total", Float32, queue_size=10)
+    def callback(self, unit):
 		
-		self.pub.publish(turnout)
-		rospy.loginfo("input data: %lf feet. output data: %lf %s", data.data, turnout, self.mode)
+        if rospy.has_param("/value"):
+            self.mode = rospy.get_param("/value")
+        else:
+            self.mode = 'meters'
+    			
+        if self.mode == 'smoots':
+            turnout = data.data * 1.7018
+        elif self.mode == 'feet':
+            turnout = data.data
+        else:
+            turnout = data.data * 3.2808
+		
+            self.pub.publish(turnout)
+            rospy.loginfo("input data: %lf feet. output data: %lf %s", data.data, turnout, self.mode)
 		
 if __name__ == '__main__':
 	rospy.init_node('homework3', anonymous=True)
