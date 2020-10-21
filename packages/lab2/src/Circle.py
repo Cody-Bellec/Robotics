7375d922b1174d92c740b07b53e4693c1e08ab00
Cody Bellec <Cody_Bellec@student.uml.edu>
2020-10-21 14:59:53 -0400

    packages/lab2/src/Circle.py

--- packages/lab2/src/Circle.py
+++ packages/lab2/src/Circle.py
@@ -4,11 +4,11 @@ import rospy
 from std_msgs.msg import Float32
 from duckietown_msgs.msg import Twist2DStamped
 
-class Homework3:
+class Circle:
     def __init__(self):
         self.pub = rospy.Publisher("car_cmd_switch_node/cmd", Twist2DStamped, queue_size=10)
 
-    def callback(self):
+    def callback(self, velocity, turnrate):
         velocity = 1.00
         turnrate = 4.5
         
@@ -19,11 +19,17 @@ class Homework3:
         self.pub.publish(turnout)
 
 if __name__ == '__main__':
-    t = Talker()
-        rospy.init_node('talker', anonymous=True)
-        rate = rospy.Rate(1) # 1hz
-        while not rospy.is_shutdown():
-            t.talk()
-            rate.sleep()
+    try:
+        c = Circle()
+        rospy.init_node('circle', anonymous=True)
+        c.callback(1.00, 4.5)
+        drive = c.callback * 4
+        c.callback(0, 0)
+        
+        
+       #rate = rospy.Rate(1) # 1hz
+       #while not rospy.is_shutdown():
+           #c.callback()
+           #rate.sleep()
     except rospy.ROSInterruptException:
         pass
