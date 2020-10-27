b850f98646b8973baa32cf7755e1d1e6a3fbc36b
Cody Bellec <Cody_Bellec@student.uml.edu>
2020-10-26 22:40:54 -0400

    packages/homework5/src/PIDclasshw5.py

---
+++ packages/homework5/src/PIDclasshw5.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import time
+from std_msgs.msg import Float32
+
+class PID:
+    def __init__(self, firstError, firstvelocity):
+        self.velocity = firstvelocity
+        self.KP = 0.15
+        self.KI = 0.01
+        self.KD = 0.4
+        
+        global t1
+        t1 = time.time()
+        global lastError
+        lastError = firstError
+        
+    def changeGainz(self, KPc, KIc, KDc):
+        self.KP = KPc
+        self.KI = KIc
+        self.KD = KDc
+    
+    def calc(self, error):
+        alphaT = time.time()-t1
+        
+        self.signal = (self.KP*error) + (self.KI * alphaT) + (self.KD *((error-lastError)/alphaT))
+        self.lastError = error
+        
+        return self.signal
+    
