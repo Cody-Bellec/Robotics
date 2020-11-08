3e4c9bd8d681f9102550d6b5a56fa02139c237f8
Cody Bellec <Cody_Bellec@student.uml.edu>
2020-11-07 22:05:07 -0500

    packages/lab3/src/

---
+++ packages/lab3/src/Lane_Followlab3.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+
+import rospy
+from std_msgs.msg import Float32
+from PIDclass import PID
+from duckietown_msgs.msg import Twist2DStamped, LanePose
+
+
+class lab3:
+    def __init__(self):
+        global controller1
+        global controller2
+        
+        controller1 = PID(0)
+        controller1.setGains(10,0,0)
+        
+        controller2 = PID(0)
+        controller2.setGains(4,0,0)
+        
+        #if rospy.has_param("/controller_ready"):
+        #    rospy.set_param("/controller_ready", 'true')       
+    
+        rospy.Subscriber("/MasonBot/lane_filter_node/lane_pose", LanePose, self.callback)
+        self.pub = rospy.Publisher('/MasonBot/car_cmd_switch_node/cmd', Twist2DStamped, queue_size=10)
+        
+    
+    
+    def callback(self, position):
+        
+        rospy.logwarn("Mason Pratte lane following program")
+
+        self.output = Twist2DStamped() 
+        self.output.v = 0.25
+             
+        self.angle = -controller1.calc(position.phi) #phi +, angle -
+        self.distance = -controller2.calc(position.d-0.07) # d +, angle -
+        
+        #angle calculation based on distance to center and angle 
+        self.output.omega = self.angle + self.distance
+        
+         
+        if self.output.omega >8:
+            self.output.omega = 8
+
+        if self.output.omega < -8:
+            self.output.omega = -8
+
+
+        #angle calculation based on distance to center and angle 
+        self.output.omega = self.angle + self.distance
+        
+        self.pub.publish(self.output)
+      
+           
+    
+if __name__ == '__main__':
+    rospy.init_node('laneFollow', anonymous=True)
+    lab3()
+    rospy.spin()
