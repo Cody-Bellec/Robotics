28cbcf95e63532e15b7098faeabacbdfdd28835d
Cody Bellec <Cody_Bellec@student.uml.edu>
2021-03-11 19:54:28 -0500

    .~lock.Homework8.odt#

--- packages/homework8/src/homework8.py
+++ packages/homework8/src/homework8.py
@@ -12,39 +12,39 @@ class ImageProcess:
         self.bridge = CvBridge()
         rospy.Subscriber("/image", Image, self.processing)
         self.pubcrop = rospy.Publisher("/image_cropped", Image, queue_size=10)
-        self.pubwhite = rospy.Publisher("/image_white", Image, queue_size=10)
-        self.pubyellow = rospy.Publisher("/image_yellow", Image, queue_size=10)
+        self.pubw = rospy.Publisher("/image_white", Image, queue_size=10)
+        self.puby = rospy.Publisher("/image_yellow", Image, queue_size=10)
         
     def processing(self, msg):
     
         #cut off top half
         cv_img1 = self.bridge.imgmsg_to_cv2(msg, "bgr8")
-        height = cv_img1.shape[0]
-        width = cv_img1.shape[1]
-        cropped = cv_img1[int(height/2):height, 0:width]
+        height = cv_image1.shape[0]
+        width = cv_image1.shape[1]
+        cropped = cv_image1[int(height/2):height, 0:width]
         roscropped = self.bridge.cv2_to_imgmsg(cropped, "bgr8")
         self.pubcrop.publish(roscropped)
         #
         
         
-        #
+        
         cv2cropped = cv2.cvtColor(cropped, cv2.COLOR_BGR2HSV)
-        white_filtered = cv2.inRange(cv2cropped, (40,0,240),(180,255,255))
-        wf = self.bridge.cv2_to_imgmsg(white_filtered, "mono8")
-        self.pubwhite.publish(wf)
-        #white filtering
-        #
+        w_filter = cv2.inRange(cv2cropped, (40,0,240),(180,255,255))
+        wf = self.bridge.cv2_to_imagemsg(white_filter, "mono8")
+        self.pubw.publish(wf)
+        #w-filtering
         
         
-        #
-        yellow_filtered = cv2.inRange(cv2cropped, (20, 100, 100),(180,255,255))
-        yf = self.bridge.cv2_to_imgmsg(yellow_filtered, "mono8")
-        self.pubyellow.publish(yf)
-        #yellow filtering
-        #
+        
+        
+        y_filter = cv2.inRange(cv2cropped, (20, 100, 100),(180,255,255))
+        yf = self.bridge.cv2_to_imagemsg(yellow_filter, "mono8")
+        self.puby.publish(yf)
+        #y-filtering
+        
 
 if __name__=="__main__":
-    # initialize our node and create a publisher as normal
+    # initialize node and create a publisher
     rospy.init_node("homework8", anonymous=True)
     img_flip = ImageProcess()
     rospy.spin()
